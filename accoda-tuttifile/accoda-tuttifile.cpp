2c6791344ea0ec0afba5cdcc1586233ad2fef9e8
Spaccini <Spaccini@LAPTOP-4H055C7R>
2024-01-05 11:50:41 +0100

    Aggiungere i file di progetto.

---
+++ accoda-tuttifile/accoda-tuttifile.cpp
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int accodaTuttiFile(const char* fileConNomi, const char* fileOutput);
+
+int main()
+{
+	int n = accodaTuttiFile("elenco.txt", "file.txt.output");
+	printf("%d", n);
+	return 0; 
+}
+
+int accodaTuttiFile(const char* fileConNomi, const char* fileOutput)
+{
+	FILE* fnomi, * fout; 
+
+	if ((fnomi = fopen(fileConNomi, "r")) == NULL)
+	{
+		return 0; 
+	}
+	if ((fout = fopen(fileOutput, "wb")) == NULL)
+	{
+		return 0; 
+	}
+	
+	size_t n = 0 ;
+	char filename[255];
+	while (fgets(filename, 255, fnomi) != NULL)
+	{
+		if (filename[strlen(filename) - 1] == '\n')
+		{
+			filename[strlen(filename) - 1] = '\0'; //sposto il \0 perche fgets mi legge anche il \n
+		}
+		FILE* f = fopen(filename, "rb");
+		if (f == NULL)
+			continue;
+		int c; 
+		while ((c = fgetc(f)) != EOF)
+			fputc(c, fout);
+
+		fclose(f);
+		n++;
+	}
+	fclose(fnomi);
+	fclose(fout);
+	return n; 
+}
